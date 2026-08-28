31745ddd6a92009ea5407f39481f375cfac41dd1
Sarah Lucia <sda.lucia@gmail.com>
2026-08-27 19:11:20 -0500

    Continuing dataset build: traced the images' paths and shuffled for later split, left additional comms

--- cancernet/config.py
+++ cancernet/config.py
@@ -1,13 +1,13 @@
-import os
+import os 
 
-INPUT_DATASET = "C:/Users/sdalu/Downloads/Projects/CancerNet/breast-cancer-classification/breast-cancer-classification/datasets/original"
+INPUT_DATASET = "datasets/original" # path to where you store the Kaggle dataset
 
-BASE_PATH = "datasets/idc"
+BASE_PATH = "datasets/idc" # new dataset to be built
 TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
 VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
 TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
 
-TRAIN_SPLIT = 0.8
+TRAIN_SPLIT = 0.8 # 80% of the images will be used to train the model, the other 20% for val check and testing (10% each)
 VAL_SPLIT = 0.1
 
 print("Configuration successfull:", BASE_PATH)
