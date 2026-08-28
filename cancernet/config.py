9a87ea5a0e566080ef282a3ae19590854ab091d3
Sarah Lucia <sda.lucia@gmail.com>
2026-08-28 14:16:54 -0500

    Dataset split: assigned the paths to the new datasets and verified the original was split properly.

--- cancernet/config.py
+++ cancernet/config.py
@@ -7,7 +7,7 @@ TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
 VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
 TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
 
-TRAIN_SPLIT = 0.8 # 80% of the images will be used to train the model, the other 20% for val check and testing (10% each)
+TRAIN_SPLIT = 0.8 # 80% of the images will be used to train the model (10% of this will be used to validate), and the remaining 20% used for testing 
 VAL_SPLIT = 0.1
 
 print("Configuration successfull:", BASE_PATH)
--- dataset_split_build.py
+++ dataset_split_build.py
@@ -1,16 +1,36 @@
 # Since we are building new paths for each image and shuffling them to have an adequate split, we will need the following:
 
-from cancernet import config # to get the config for the split we made earlier in a seperate file
-from imutils import paths # to locate the paths of the images
-import random, shutil, os # to shuffle the images and manage files and paths directly in your system
+from cancernet import config
+from imutils import paths 
+import random, shutil, os 
 
-paths_original = list(paths.list_images(config.INPUT_DATASET))
+paths_original = list(paths.list_images(config.INPUT_DATASET)) 
 random.seed(42)
 random.shuffle(paths_original)
 
 print("Total images found:", len(paths_original))
 
-print("First three shuffled paths:")
+print("First three shuffled paths:") # just to make sure paths are working properly
 for paths in paths_original[:3]:
     print(paths)
-    
+
+index = int(len(paths_original) * config.TRAIN_SPLIT) # refer to the comm in config.py to understand the split
+train_paths = paths_original[:index]
+test_paths = paths_original[index:]
+
+index = int(len(train_paths) * config.VAL_SPLIT)
+val_paths = train_paths[:index]
+train_paths = train_paths[index:]
+
+dataset_split = [("training", train_paths, config.TRAIN_PATH), 
+                 ("validating", val_paths, config.VAL_PATH),
+                 ("testing", test_paths, config.TEST_PATH)
+]
+
+print("Val images:", len(val_paths)) # just to make sure the split was done properly 
+print("Test images:", len(test_paths))
+print("Train images:", len(train_paths))
+print("Check it all adds up:", len(val_paths) + len(test_paths) + len(train_paths))
+
+
+
