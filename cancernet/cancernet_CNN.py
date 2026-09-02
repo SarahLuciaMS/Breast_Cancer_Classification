8f5ed29684ab9183ead13cd8932b27be898c1c1d
Sarah Lucia <sda.lucia@gmail.com>
2026-09-01 19:07:45 -0500

    Building the CNN: Establishing libs to be used.

---
+++ cancernet/cancernet_CNN.py
@@ -0,0 +1,4 @@
+from tensorflow.keras.models import Sequential # this will be the kind of layering we will use
+from tensorflow.keras.layers import BatchNormalization, Activation, MaxPooling2D, SeparableConv2D, Flatten, Dense, Dropout # config for our layers 
+from tensorflow.keras import backend as K # usefull in case custom operations are needed
+
