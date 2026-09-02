7ca67bc67d7959d3f903deb8b2f0bcde1a88cc57
Sarah Lucia <sda.lucia@gmail.com>
2026-09-01 19:36:46 -0500

    Creating the model class for training.

--- cancernet/cancernet_CNN.py
+++ cancernet/cancernet_CNN.py
@@ -2,3 +2,15 @@ from tensorflow.keras.models import Sequential # this will be the kind of layeri
 from tensorflow.keras.layers import BatchNormalization, Activation, MaxPooling2D, SeparableConv2D, Flatten, Dense, Dropout # config for our layers 
 from tensorflow.keras import backend as K # usefull in case custom operations are needed
 
+class CancerNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = Sequential()
+        shape = (height, width, depth)
+        channelDim = -1 # makes sure the batch normalization is applied to the channels (depth) of the shape 
+
+        if K.image_data_format == "channels_first": # Incase for any reason this will be run with anythign other than TensorFlow
+            shape = (depth, width, height)
+            channelDim = 1
+
+        
