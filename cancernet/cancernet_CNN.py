ff70d8c70b75a54586d02334c3d06e35fc47ba24
Sarah Lucia <sda.lucia@gmail.com>
2026-09-02 21:25:55 -0500

    Finished the model.

--- cancernet/cancernet_CNN.py
+++ cancernet/cancernet_CNN.py
@@ -13,7 +13,7 @@ class CancerNet:
             shape = (depth, width, height)
             channelDim = 1
 
-        model.add(SeparableConv2D(32, (3,3), padding = "same", input_shape = shape)) # start of the 3*3 CNN model
+        model.add(SeparableConv2D(32, (3,3), padding = "same", input_shape = shape)) 
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis = channelDim))
         model.add(MaxPooling2D(pool_size = (2, 2)))
@@ -40,7 +40,16 @@ class CancerNet:
         model.add(MaxPooling2D(pool_size = (2, 2)))
         model.add(Dropout(0.25))
 
-        
+        model.add(Flatten())
+        model.add(Dense(256))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        return model
 
 
         
