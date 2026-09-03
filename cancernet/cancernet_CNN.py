c4e8b2b9e91ef46603160b480a00de684c010e5d
Sarah Lucia <sda.lucia@gmail.com>
2026-09-02 20:56:45 -0500

    Making of the first layer of the CNN mode.

--- cancernet/cancernet_CNN.py
+++ cancernet/cancernet_CNN.py
@@ -13,4 +13,9 @@ class CancerNet:
             shape = (depth, width, height)
             channelDim = 1
 
+        model.add(SeparableConv2D(32, (3,3), padding = "same", input_shape = shape)) # first neuron 
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = channelDim))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.25))
         
